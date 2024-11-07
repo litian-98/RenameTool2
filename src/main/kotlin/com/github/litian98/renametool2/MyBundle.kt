edb3514ce96be90d40286378a55c3e2ad901e268
GitHub Action <action@github.com>
2024-11-07 02:18:36 +0000

    Template cleanup

--- src/main/kotlin/com/github/litian98/renametool2/MyBundle.kt
+++ src/main/kotlin/com/github/litian98/renametool2/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.litian98.renametool2
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/litian98/renametool2/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/litian98/renametool2/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.litian98.renametool2.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
